d1bf20fac6b1434c7eba99f36d86cc7e1fd2a9af
Alfonso Manuel Baena González <alfonsombaena@gmail.com>
2025-01-11 20:22:07 +0100

    :sparkles: feat: cofing build and add no-print behaviuor

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  build: {
+    assetsPrefix: '.'
+  },
+  integrations: [tailwind()],
 });
