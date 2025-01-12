cf1deaae7bdcb67e79f083bf279eaef676568588
Alfonso Manuel Baena González <alfonsombaena@gmail.com>
2025-01-13 00:49:58 +0100

    :sparkles: feat: fine tunning astro config and add vercel anaytics and insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,27 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alfonsobaena.dev',
   build: {
     assetsPrefix: '.'
   },
   integrations: [tailwind()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  vite: {
+    define: {
+      'import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID': JSON.stringify(
+        process.env.VERCEL_ANALYTICS_ID,
+      ),
+      'import.meta.env.PUBLIC_VERCEL_URL': JSON.stringify(
+        process.env.VERCEL_URL,
+      ),
+    },
+  },
 });
